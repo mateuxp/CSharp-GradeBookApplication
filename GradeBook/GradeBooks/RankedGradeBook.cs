10264a66106b0a062ee6082934c2fee46c70f0c7
mateuxp <mateuxp@gmail.com>
2022-11-15 14:55:59 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,5 +9,11 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("You need more than 5 students");
+            return base.GetLetterGrade(averageGrade);
+        }
     }
-}
