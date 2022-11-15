31376b9337b0adce25bd2d6f5bf9576b118d44fc
mateuxp <mateuxp@gmail.com>
2022-11-15 14:50:12 +0100

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -269,9 +269,5 @@ namespace GradeBook.GradeBooks
             return JsonConvert.DeserializeObject(json, gradebook);
         }
     }
-
-    public class RankedGradeBook
-    {
-        
-    }
+    
 }
