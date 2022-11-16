2fa778c12a49a01c1ce0cb7ebcde354efab187a0
mateuxp <mateuxp@gmail.com>
2022-11-16 15:21:08 +0100

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -42,7 +42,7 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             var type = parts[2].ToLower();
             var weighted = bool.Parse(parts[3]);
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
             if (type == "standard" )
             {
                 gradeBook = new StandardGradeBook(name, weighted);
